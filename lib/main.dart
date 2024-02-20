b7cb50194d65882d675daf4590d537a9d155b83f
Wasim Aldimashki <aldemashky2001@gmail.com>
2024-02-20 12:13:11 +0300

    create notes app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +15,16 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const NotesApp(),
     );
+  }
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
