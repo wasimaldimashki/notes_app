1d54ed7fe8a4864bfc8f833bab5e9bb7ee503d46
Wasim Aldimashki <aldemashky2001@gmail.com>
2024-02-21 22:10:53 +0300

    provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants/constants.dart';
 import 'package:notes_app/core/class/theme_mod.dart';
 import 'package:notes_app/core/models/note_model.dart';
+import 'package:notes_app/cubits/add_note/add_note_cubit.dart';
 import 'package:notes_app/cubits/theme/theme_cubit.dart';
 import 'package:notes_app/theme/app_theme.dart';
 import 'package:notes_app/views/home.dart';
@@ -21,18 +22,25 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ThemeCubit(),
-      child: BlocBuilder<ThemeCubit, ThemeModeEnum>(
-        builder: (context, themeMode) {
-          final ThemeData themeData = themeMode == ThemeModeEnum.Light
-              ? LightTheme.themeData
-              : DarkTheme.themeData;
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Notes Application',
-            theme: themeData,
-            home: const HomePage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeCubit>(create: (context) => ThemeCubit()),
+        BlocProvider<AddNoteCubit>(create: (context) => AddNoteCubit()),
+      ],
+      child: Builder(
+        builder: (context) {
+          return BlocBuilder<ThemeCubit, ThemeModeEnum>(
+            builder: (context, themeMode) {
+              final ThemeData themeData = themeMode == ThemeModeEnum.Light
+                  ? LightTheme.themeData
+                  : DarkTheme.themeData;
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Notes Application',
+                theme: themeData,
+                home: const HomePage(),
+              );
+            },
           );
         },
       ),
